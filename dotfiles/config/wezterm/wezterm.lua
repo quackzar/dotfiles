039c0cbc7aae77aafe499fb2f65e032064e72bce
Mikkel Wienberg Madsen <mikkel.wienberg@gmail.com>
2022-07-11 11:22:01 +0200

    add wezterm support

--- dotfiles/config/wezterm/wezterm.lua
+++ dotfiles/config/wezterm/wezterm.lua
@@ -1,1 +1,6 @@
-enable_csi_u_key_encoding = true
+local wezterm = require 'wezterm';
+return {
+    font = wezterm.font("JetBrains Mono"),
+    color_scheme = "Batman",
+    enable_csi_u_key_encoding = true
+}
